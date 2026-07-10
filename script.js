0e213536223f2c6ae2701ad9c17e2da3d2dff2ef
mj4433314-cmyk <mj4433314@gmail.com>
2026-07-10 14:18:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function showWallet() {
+    alert(
+`TRC20 Wallet
 
+THJN4XBKxbZNYsw7YAHmNiZSupn3kVEDFP`
+    );
+}
+
+document.querySelectorAll(".activate-btn").forEach(btn => {
+    btn.addEventListener("click", function () {
+        alert(
+`Deposit Address (TRC20)
+
+THJN4XBKxbZNYsw7YAHmNiZSupn3kVEDFP`
+        );
+    });
+});
+
+document.querySelectorAll(".nav-item").forEach(item => {
+    item.addEventListener("click", function () {
+
+        document.querySelectorAll(".nav-item")
+        .forEach(i => i.classList.remove("active"));
+
+        this.classList.add("active");
+
+    });
+});
