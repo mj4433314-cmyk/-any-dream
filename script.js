8e39891794ad35ccf832ed181fd61319a96b5626
mj4433314-cmyk <mj4433314@gmail.com>
2026-07-10 14:48:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
-function showWallet() {
-    alert(
-`TRC20 Wallet
+const phone=document.getElementById("phone");
+const code=document.getElementById("countryCode");
+const loginBtn=document.getElementById("loginBtn");
+const createBtn=document.getElementById("createBtn");
+const loading=document.getElementById("loading");
 
-THJN4XBKxbZNYsw7YAHmNiZSupn3kVEDFP`
-    );
+phone.addEventListener("input",function(){
+
+if(this.value.startsWith("077")){
+code.innerHTML="🇮🇶 +964";
+}else{
+code.innerHTML="";
 }
 
-document.querySelectorAll(".activate-btn").forEach(btn => {
-    btn.addEventListener("click", function () {
-        alert(
-`Deposit Address (TRC20)
-
-THJN4XBKxbZNYsw7YAHmNiZSupn3kVEDFP`
-        );
-    });
 });
 
-document.querySelectorAll(".nav-item").forEach(item => {
-    item.addEventListener("click", function () {
+loginBtn.onclick=function(){
 
-        document.querySelectorAll(".nav-item")
-        .forEach(i => i.classList.remove("active"));
+alert("عذراً، ليس لديك حساب.");
 
-        this.classList.add("active");
+};
 
-    });
-});
+createBtn.onclick=function(){
+
+const inputs=document.querySelectorAll("input");
+
+for(let i=0;i<inputs.length;i++){
+
+if(inputs[i].value==""){
+
+alert("يرجى إكمال جميع الحقول");
+
+return;
+
+}
+
+}
+
+loading.style.display="flex";
+
+setTimeout(function(){
+
+loading.innerHTML="<h2>✅ تم إنشاء الحساب بنجاح</h2>";
+
+setTimeout(function(){
+
+window.location.href="home.html";
+
+},2000);
+
+},10000);
+
+};
